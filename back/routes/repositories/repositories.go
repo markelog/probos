867e2ba5446f2cbf5775d0663e26ace698035a55
Oleg Gaidarenko <markelog@gmail.com>
2020-01-18 23:55:13 +0300

    Back: remove validation of repos from route

--- back/routes/repositories/repositories.go
+++ back/routes/repositories/repositories.go
@@ -5,7 +5,6 @@ import (
 	"github.com/kataras/iris/v12"
 	controller "github.com/markelog/probos/back/controllers/repository"
 	"github.com/sirupsen/logrus"
-	"github.com/xeipuuv/gojsonschema"
 )
 
 type postRepository struct {
@@ -13,61 +12,11 @@ type postRepository struct {
 	Repository string `json:"repository"`
 }
 
-var schema = gojsonschema.NewStringLoader(`{
-	"type": "object",
-	"properties": {
-		"name": {"type": "string", "minLength": 1},
-		"repository": {"type": "string", "minLength": 1}
-	},
-	"required": ["name", "repository"]
-}`)
-
-func validate(params *postRepository) (*gojsonschema.Result, *iris.Map) {
-	var (
-		paramsLoader = gojsonschema.NewGoLoader(params)
-		check, _     = gojsonschema.Validate(schema, paramsLoader)
-
-		errors  []string
-		payload *iris.Map
-	)
-
-	if !check.Valid() {
-		for _, desc := range check.Errors() {
-			errors = append(errors, desc.String())
-		}
-
-		payload = &iris.Map{"errors": errors}
-
-		return check, payload
-	}
-
-	return check, nil
-}
-
 // Up Repository route
 func Up(app *iris.Application, db *gorm.DB, log *logrus.Logger) {
 	app.Post("/repositories", func(ctx iris.Context) {
 		var params postRepository
 		ctx.ReadJSON(&params)
-
-		validation, errors := validate(&params)
-
-		if !validation.Valid() {
-			log.WithFields(logrus.Fields{
-				"Repository": params.Name,
-				"repository": params.Repository,
-			}).Error("Params are not valid")
-
-			ctx.StatusCode(iris.StatusBadRequest)
-			ctx.JSON(iris.Map{
-				"status":  "failed",
-				"message": "Params are not valid",
-				"payload": errors,
-			})
-
-			return
-		}
-
 		ctrl := controller.New(db)
 		result, err := ctrl.Create(params.Name, params.Repository)
 
